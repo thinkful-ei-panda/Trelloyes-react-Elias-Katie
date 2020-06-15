f900527f28c480d423492a1fc9482911932b3895
Katie Carroll <ktcarroll4@gmail.com>
2020-06-15 16:04:13 -0500

    completed assignment but page not rendering

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./Card.css";
 
-function Cards(props) {
-    return (
-        <div className="Card">
-            <h3>{props.title}</h3>
-            <p>{props.content}</p>
-        </div>
-    );
+export default function Cards(props) {
+  return (
+    <div className="Card">
+      <button type="button">delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  );
 }
-
