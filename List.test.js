cf77d8e2433c9f1eb803dc1d27442b25e70c591a
Katie Carroll <ktcarroll4@gmail.com>
2020-06-16 11:13:16 -0500

    trying so hard

--- List.test.js
+++ List.test.js
@@ -1,7 +1,8 @@
 //smoke test
 //list
-import React from "react";
 import ReactDOM from "react-dom";
+import React from "react";
+import Card from "./Card";
 import { render } from "@testing-library/react";
 import List from "./List";
 
@@ -10,16 +11,23 @@ describe("<List />", () => {
     const div = document.createElement("div");
 
     // Pass required props in the usual way
-    ReactDOM.render(
-      <div className="List-cards">
-        {props.cards.map((card) => (
-          <Card key={card.id} title={card.title} content={card.content} />
-        ))}
-        <button type="button" className="List-add-button">
-          + Add Random Card
-        </button>
-      </div>
-    );
+    ReactDOM.render(function List(props) {
+      return (
+        <section className="List">
+          <header className="List-header">
+            <h2>{props.header}</h2>
+          </header>
+          <div className="List-cards">
+            {props.cards.map((card) => (
+              <Card key={card.id} title={card.title} content={card.content} />
+            ))}
+            <button type="button" className="List-add-button">
+              + Add Random Card
+            </button>
+          </div>
+        </section>
+      );
+    });
 
     ReactDOM.unmountComponentAtNode(div);
   });
