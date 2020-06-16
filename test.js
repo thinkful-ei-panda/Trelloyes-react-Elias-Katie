f1fb7d7ec7dbcce89f6522b9d80b6c24d9e5f575
Katie Carroll <ktcarroll4@gmail.com>
2020-06-16 10:40:47 -0500

    smoke test

--- test.js
+++ test.js
@@ -1,65 +1,16 @@
 //smoke test
 //list
-import React from 'react';
+import React from "react";
 // make the ReactDOM available, necessary for rendering the component
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 // make the App component available
-import App from './App';
+import App from "./App";
 // this is the test case
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   // first create a DOM element to render the component into
-  const div = document.createElement('div');
+  const div = document.createElement("main");
   // render the component, this is the actual test, if something is wrong it will fail here
   ReactDOM.render(<App />, div);
   // clean up code
   ReactDOM.unmountComponentAtNode(div);
 });
-
-//card
-import React from 'react';
-// make the ReactDOM available, necessary for rendering the component
-import ReactDOM from 'react-dom';
-// make the App component available
-import App from './App';
-// this is the test case
-it('renders without crashing', () => {
-  // first create a DOM element to render the component into
-  const div = document.createElement('div');
-  // render the component, this is the actual test, if something is wrong it will fail here
-  ReactDOM.render(<App />, div);
-  // clean up code
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-//snapshot test
-//list
-
-import React from 'react';
-import './Messages.css';
-export default function Messages(props) {
-  const badge = props.unread 
-    ? <div className="unread_count">{props.unread}</div> 
-    : null;
-  return (
-    <div className="messages">
-      {props.name} 
-      {badge}
-    </div>  
-  );
-}
-//card
-
-import React from 'react';
-import './Messages.css';
-export default function Messages(props) {
-  const badge = props.unread 
-    ? <div className="unread_count">{props.unread}</div> 
-    : null;
-  return (
-    <div className="messages">
-      {props.name} 
-      {badge}
-    </div>  
-  );
-}
-
